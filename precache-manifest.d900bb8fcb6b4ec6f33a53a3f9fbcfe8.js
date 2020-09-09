7323577a5d5d34ccd483b1ccef2ed21cd7a41279
oxbe <natthakittasts@mail.com>
2020-09-09 20:44:13 +0700

    Updates

---
+++ precache-manifest.d900bb8fcb6b4ec6f33a53a3f9fbcfe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646d5d34cba9d90bb9ffc6dbfecb8e25",
+    "url": "/projectwe-web/index.html"
+  },
+  {
+    "revision": "0a27bd4451fcc6d029e4",
+    "url": "/projectwe-web/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f2104b2855829fdad1d0",
+    "url": "/projectwe-web/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "0a27bd4451fcc6d029e4",
+    "url": "/projectwe-web/static/js/2.c1ef3d34.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/projectwe-web/static/js/2.c1ef3d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2104b2855829fdad1d0",
+    "url": "/projectwe-web/static/js/main.c8dd1340.chunk.js"
+  },
+  {
+    "revision": "8c6d962b09087de36f34",
+    "url": "/projectwe-web/static/js/runtime-main.60d3249e.js"
+  }
+]);
